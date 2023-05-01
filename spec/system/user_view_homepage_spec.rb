ae33367889c414e8aeaf39f76fbf9c59610ed6a2
Renan Antonio da Silva <renanras@gmail.com>
2023-05-01 17:30:06 -0300

    Teste 2 ok; Cadastro de Galpões; e visualização das informações

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -11,4 +11,26 @@ describe 'Usuário visita tela inicial' do
         expect(page).to have_content('Galpões & Estoque')
 
     end
+
+    it 'e vê os galpões cadastrados' do
+        # Arrange
+        # Cadastrar 2 galpões: Rio e Maceio
+        Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000)
+        Warehouse.create(name: 'Maceio', code: 'MCZ', city: 'Maceio', area: 50_000)
+
+        # Act
+        visit('/')
+
+        # Assert
+        # Garantir que se veja na tela os galpões Rio e Maceio
+        expect(page).to have_content('Rio')
+        expect(page).to have_content('Código: SDU')
+        expect(page).to have_content('Cidade: Rio de Janeiro')
+        expect(page).to have_content('60000 m2')
+
+        expect(page).to have_content('Maceio')
+        expect(page).to have_content('Código: MCZ')
+        expect(page).to have_content('Cidade: Maceio')
+        expect(page).to have_content('50000 m2')
+    end
 end
