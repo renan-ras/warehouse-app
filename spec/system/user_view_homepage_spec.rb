9e03536579acf93c321308830fcb3de2b915f7f9
Renan Antonio da Silva <renanras@gmail.com>
2023-05-02 15:51:04 -0300

    User vê detalhes de galpão e volta à tela inicial

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -1,47 +1,47 @@
 require "rails_helper"
 
 describe 'Usuário visita tela inicial' do
-    it 'e vê o nome da app' do
-        # Arrange
-        
-        # Act
-        visit('/')
+  it 'e vê o nome da app' do
+    # Arrange
+    
+    # Act
+    visit('/')
 
-        # Assert
-        expect(page).to have_content('Galpões & Estoque')
+    # Assert
+    expect(page).to have_content('Galpões & Estoque')
 
-    end
+  end
 
-    it 'e vê os galpões cadastrados' do
-        # Arrange
-        # Cadastrar 2 galpões: Rio e Maceio
-        Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000)
-        Warehouse.create(name: 'Maceio', code: 'MCZ', city: 'Maceio', area: 50_000)
+  it 'e vê os galpões cadastrados' do
+    # Arrange
+    # Cadastrar 2 galpões: Rio e Maceio
+    Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000)
+    Warehouse.create(name: 'Maceio', code: 'MCZ', city: 'Maceio', area: 50_000)
 
-        # Act
-        visit('/')
+    # Act
+    visit('/')
 
-        # Assert
-        expect(page).not_to have_content('Não existem galpões cadastrados')
-        # Garantir que se veja na tela os galpões Rio e Maceio
-        expect(page).to have_content('Rio')
-        expect(page).to have_content('Código: SDU')
-        expect(page).to have_content('Cidade: Rio de Janeiro')
-        expect(page).to have_content('60000 m2')
+    # Assert
+    expect(page).not_to have_content('Não existem galpões cadastrados')
+    # Garantir que se veja na tela os galpões Rio e Maceio
+    expect(page).to have_content('Rio')
+    expect(page).to have_content('Código: SDU')
+    expect(page).to have_content('Cidade: Rio de Janeiro')
+    expect(page).to have_content('60000 m2')
 
-        expect(page).to have_content('Maceio')
-        expect(page).to have_content('Código: MCZ')
-        expect(page).to have_content('Cidade: Maceio')
-        expect(page).to have_content('50000 m2')
-    end
+    expect(page).to have_content('Maceio')
+    expect(page).to have_content('Código: MCZ')
+    expect(page).to have_content('Cidade: Maceio')
+    expect(page).to have_content('50000 m2')
+  end
 
-    it 'e não existem galpões cadastrados' do
-        # Arrange
-        
-        # Act
-        visit('/')
+  it 'e não existem galpões cadastrados' do
+    # Arrange
+    
+    # Act
+    visit('/')
 
-        # Assert
-        expect(page).to have_content('Não existem galpões cadastrados')
-    end
+    # Assert
+    expect(page).to have_content('Não existem galpões cadastrados')
+  end
 end
---
+++ spec/system/user_view_warehouse_details_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe 'Usuário vê detalhes de um galpão' do
+  it 'e vê informações adicionais' do
+    # Arrange
+    Warehouse.create(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos',
+      area: 100_000, address: 'Avenida do Aeroporto, 1000', zip: '15000-000',
+      description: 'Galpão destinado para cargas internacionais')
+
+    
+    # Act
+    visit('/')
+    click_on('Aeroporto SP')
+
+    # Assert
+    expect(page).to have_content('Galpão GRU')
+    expect(page).to have_content('Nome: Aeroporto SP')
+    expect(page).to have_content('Cidade: Guarulhos')
+    expect(page).to have_content('Área: 100000 m2')
+    expect(page).to have_content('Avenida do Aeroporto, 1000 CEP: 15000-000')
+    expect(page).to have_content('Galpão destinado para cargas internacionais')
+  end
+
+  it 'Volta para a tela inicial' do
+    # Arrange
+    Warehouse.create(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos',
+      area: 100_000, address: 'Avenida do Aeroporto, 1000', zip: '15000-000',
+      description: 'Galpão destinado para cargas internacionais')
+    # Act
+    visit('/')
+    click_on('Aeroporto SP')
+    click_on('Voltar')
+    # Assert
+    expect(current_path).to eq('/')
+  end
+end
